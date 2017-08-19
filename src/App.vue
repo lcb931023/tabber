d0b796200b4c7d0adf7a4fe241b6fda29d214a98
Changbai Li <lcb931023@gmail.com>
2017-08-19 01:21:36 -0700

    Initial Commit - scaffolding

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1> Tabber </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Noto', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
